d8b44ab8ea09e91ee5472842010fe33a88d9fc01
Sreevardhan_reddy <vardhan114433@gmail.com>
2025-05-15 18:46:16 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+document.write("Hello World");
+
+
+
+
+
+console.log(10);
+console.log(20);
+
+//using var container
+var student_age=10;
+var student_name="John Doe";
+
+console.log(student_age);
+console.log(student_name);
+
+//using let keyword
+let student_age1=20;
+let student_name1="Jane Doe";
+
+console.log(student_age1);
+console.log(student_name1);
+
+//using const keyword
+const student_age2=30;
+const student_name2="Jim Doe";
+console.log(student_age2);
+console.log(student_name2);
+
+
