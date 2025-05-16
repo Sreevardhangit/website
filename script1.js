a95ec56bf4cbe9080ac51cb9113d23bf250ae0e6
Sreevardhan_reddy <vardhan114433@gmail.com>
2025-05-16 08:54:56 +0530

    2 - script

---
+++ script1.js
@@ -0,0 +1,19 @@
+let age=20;//number
+console.log(age);
+
+
+var name="Sree";//string
+console.log(name);
+
+
+let isStudent=true;//boolean
+console.log(isStudent);
+
+
+let a=null;//null
+console.log(a);
+let b=undefined;//undefined
+console.log(b);
+
+let name1=Symbol("vardhan");//symbol
+console.log(name1);
