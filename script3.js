93f50faf4710ac159798e5e87779cc35fa8de83d
Sreevardhan_reddy <vardhan114433@gmail.com>
2025-05-19 21:04:35 +0530

    JavaScript array

---
+++ script3.js
@@ -0,0 +1,18 @@
+var student_name=["John", "Jane", "Jim", "Jack", "Jill"];
+var student_age=[20, 21, 22, 23, 24];
+var student_grade=["A", "B", "C", "D", "E"];
+var student_address=["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
+console.log("Student Name: " + student_name);
+console.log("Student Age: " + student_age);
+console.log("Student Grade: " + student_grade);
+console.log("Student Address: " + student_address);
+console.log(student_name[1]);
+console.log(student_age[2]);
+console.log(student_grade[3]);
+console.log(student_address[4]);
+console.log(student_name[0] + " is " + student_age[0] + " years old and lives in " + student_address[0]);
+console.log(student_name[1] + " is " + student_age[1] + " years old and lives in " + student_address[1]);
+
+console.log(student_name[2] + " is " + student_age[2] + " years old and lives in " + student_address[2]);
+console.log(student_name[3] + " is " + student_age[3] + " years old and lives in " + student_address[3]);
+console.log(student_name[4] + " is " + student_age[4] + " years old and lives in " + student_address[4]);
