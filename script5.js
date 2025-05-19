b9be9db75cccc99214b3982cb600f0dc43c7ea2b
Sreevardhan_reddy <vardhan114433@gmail.com>
2025-05-19 21:32:35 +0530

    functions_nonprimitive

---
+++ script5.js
@@ -0,0 +1,17 @@
+var cars = function () {
+    console.log("bmw");
+};
+console.log(cars);
+cars();
+console.dir(cars);
+
+
+function studentDetails(){
+    let name = "sree";
+    console.log(name);
+    let age = 22;
+    console.log(age);
+    let id = 17;
+    console.log(id);
+}
+studentDetails();
