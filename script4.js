742865c3657072dfe09b84ce4e17fd8049a93e5f
Sreevardhan_reddy <vardhan114433@gmail.com>
2025-05-19 21:11:56 +0530

    objects-nonprimitive

---
+++ script4.js
@@ -0,0 +1,27 @@
+var student ={
+    name:ServiceWorker,
+    age: 20,
+    student_id:17,
+    address: {
+        city: "New York",
+        state: "NY"
+    },
+}
+
+var student2 = {
+    name: "John Doe",
+    age: 22,
+    student_id: 18,
+    address: {
+        city: "Los Angeles",
+        state: "CA"
+    },
+}
+
+console.log(student);
+console.log(student2);
+console.log(student.name);
+console.log(student2.name);
+console.log(student.age);
+console.log(student2.age);
+console.log(student.student_id);
