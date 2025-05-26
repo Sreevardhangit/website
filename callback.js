92e034368947ee6cfc7d51770ae871171ade6286
Sreevardhan_reddy <vardhan114433@gmail.com>
2025-05-26 18:57:50 +0530

    updated

--- callback.js
+++ callback.js
@@ -9,3 +9,21 @@ calculator(function(a,b){
     console.log("The multiplication is: " + (a * b));
     console.log("The division is: " + (a / b));
 }); 
+
+setTimeout(function(){
+    console.log("This is a timeout function.");
+}, 2000);
+
+
+
+let driver=() => {
+    console.log("this is an arrow function");
+}
+driver();
+
+//return arrow function
+let books =(name,subject,price) => {
+    return `The book name is ${name}, subject is ${subject}, and the price is ${price}`;
+}
+books = books("JavaScript", "Programming", 500);
+console.log(books);
