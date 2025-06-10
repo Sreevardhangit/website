136f9d184cc9182f7863dba15f28cac9c4902a8d
Sreevardhan_reddy <vardhan114433@gmail.com>
2025-06-10 08:23:36 +0530

    class update

--- class.js
+++ class.js
@@ -66,24 +66,63 @@
 
 
 
-class Details{
-    S_name="sree";
-    S_id ="123";
+// class Details{
+//     S_name="sree";
+//     S_id ="123";
 
-    get S_name(){
-        this.S_name;
+//     get S_name(){
+//         this.S_name;
 
+//     }
+//     set S_name(value){
+//         if(typeof this.S_name == String){
+//             this.S_name = value;
+//         }
+//         else{
+//             console.log("You have entered wrong data type");
+//         }
+//     }
+// }
+// let rDetails = new Details();
+// rDetails.S_name = false;
+
+
+class Details {
+    constructor() {
+        this._S_name = 12;  // Use a private variable convention
+        this.S_id = "123";
     }
-    set S_name(value){
-        if(typeof this.S_name == String){
-            this.S_name = value;
+
+    get S_name() {
+        return this._S_name;
+    }
+
+    set S_name(value) {
+        // if (typeof value === "string") {
+        //     this._S_name = value;
+        // } else {
+        //     console.log("You have entered wrong data type");
+        // }
+        if (typeof value === "string" && /^[a-zA-Z]+$/.test(value)) {
+            this._S_name = value;
+        } else {
+            console.log("Only alphabetic characters are allowed.");
         }
-        else{
-            console.log("You have entered wrong data type");
-        }
+        // this.S_name = String(value);
     }
 }
+
 let rDetails = new Details();
-rDetails.S_name = false;
 
+console.log(rDetails.S_name);  // Outputs: sree
+rDetails.S_name = "vardhan";    
+console.log(rDetails.S_name);   // Output: vardhan
 
+rDetails.S_name = 12345;        //  Invalid input
+console.log(rDetails.S_name);   // Output: vardhan (unchanged)
+
+rDetails.S_name = "abc123";     //  Invalid input (contains numbers)
+console.log(rDetails.S_name);   // Output: vardhan
+
+rDetails.S_name = "Sree";       // Valid input (letters only)
+console.log(rDetails.S_name);  //Outpu : Sree
